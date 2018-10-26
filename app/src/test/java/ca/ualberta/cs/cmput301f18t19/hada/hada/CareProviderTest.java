278fab37d3749a52ccd7edfabf21a64ed5565ad5
Joe Potentier <joseph.potentier@gmail.com>
2018-10-26 14:44:39 -0600

    Care Provider Class Implemented
    Test and CP class implemented in the most bare-bones way.

---
+++ app/src/test/java/ca/ualberta/cs/cmput301f18t19/hada/hada/CareProviderTest.java
@@ -0,0 +1,61 @@
+package ca.ualberta.cs.cmput301f18t19.hada.hada;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CareProviderTest {
+
+    @Test
+    public void testSetUserID(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setUserID("Test CP User ID");
+        assertEquals("Test CP User ID", careProvider.getUserID());
+    }
+
+    @Test
+    public void testGetUserID(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setUserID("Test User Id");
+        String returnedID = careProvider.getUserID();
+        assertEquals("Test User Id",returnedID);
+    }
+
+
+    @Test
+    public void testSetPhoneNumber(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setPhoneNumber("7801234567");
+        assertEquals("7801234567", careProvider.getPhoneNumber());
+    }
+
+    @Test
+    public void testGetPhoneNumber(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setPhoneNumber("7801234567");
+        String returnedPhoneNumber = careProvider.getPhoneNumber();
+        assertEquals("7801234567", returnedPhoneNumber);
+    }
+
+    @Test
+    public void testSetEmailAddress(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setEmailAdress("test@ualberta.ca");
+        assertEquals("test@ualberta.ca", careProvider.getEmailAddress());
+    }
+
+    @Test
+    public void testGetEmailAddress(){
+        CareProvider careProvider = new CareProvider();
+        careProvider.setEmailAdress("test@ualberta.ca");
+        String returnedEmailAddress = careProvider.getEmailAddress();
+        assertEquals("test@ualberta.ca", returnedEmailAddress);
+    }
+
+    @Test
+    public void testGetType(){
+        CareProvider careProvider = new CareProvider();
+        assertEquals("Care Provider",careProvider.getType());
+    }
+
+}
