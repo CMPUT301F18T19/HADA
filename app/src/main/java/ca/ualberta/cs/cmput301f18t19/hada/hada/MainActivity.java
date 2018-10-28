8c44dad4edd2e84dc3ca6128e63edf7c6ecf3fd5
Unknown <joseph.potentier@gmail.com>
2018-10-27 22:40:07 -0600

    Problem Class Implemented
    Problem class and tests implemented.

--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/MainActivity.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/MainActivity.java
@@ -1,3 +1,13 @@
+/* CMPUT 301 - Fall 2018
+ *
+ * Version 1.0
+ *
+ * 2018-10-27
+ *
+ * This is a group project for CMPUT 301 course at the University of Alberta
+ * Copyright (C) 2018  Austin Goebel, Anders Johnson, Alex Li,
+ * Cristopher Penner, Joseph Potentier-Neal, Jason Robock
+ */
 package ca.ualberta.cs.cmput301f18t19.hada.hada;
 
 import android.support.v7.app.AppCompatActivity;
