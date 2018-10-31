9270bc65dc1f92480dc35d0684193528eb4285e7
Jason Robock <robockj@mymacewan.ca>
2018-10-31 14:26:05 -0600

    stuff

---
+++ app/src/test/java/ca/ualberta/cs/cmput301f18t19/hada/hada/ListManagerRecordsTest.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.cmput301f18t19.hada.hada;
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ListManagerRecordsTest {
+    @Test
+    public void testSaveRecord() {
+        Record r = new Record();
+
+    }
+}
