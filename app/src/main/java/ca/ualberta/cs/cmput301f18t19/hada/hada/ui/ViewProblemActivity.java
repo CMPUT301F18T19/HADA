0d1185775360be73701502ba89dae80c7274825c
Unknown <joseph.potentier@gmail.com>
2018-11-08 15:13:39 -0700

    Auto stash before rebase of "origin/master"

---
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/ui/ViewProblemActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.cmput301f18t19.hada.hada.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewProblemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_problem);
+    }
+}
