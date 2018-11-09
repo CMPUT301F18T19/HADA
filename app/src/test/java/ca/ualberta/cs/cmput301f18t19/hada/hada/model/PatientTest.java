aaa7c4c5c17e71c750c02d04fb94e02470bf627e
Alex Li <tianyi7@ualberta.ca>
2018-11-09 09:23:27 -0700

    Added tests for the new setters and getters for individual problems in Patient.

--- app/src/test/java/ca/ualberta/cs/cmput301f18t19/hada/hada/model/PatientTest.java
+++ app/src/test/java/ca/ualberta/cs/cmput301f18t19/hada/hada/model/PatientTest.java
@@ -57,21 +57,64 @@ public class PatientTest {
     }
 
     @Test
-    public void testSetProblems(){
+    public void testSetProblemList(){
         Patient patient = new Patient();
-        ArrayList<Problem> problems  = new ArrayList<Problem>();
-        Problem problem = new Problem();
-        patient.setProblems(problems);
-        assertEquals("Should be the ProblemListController we set it to", problems, patient.getProblems());
+        ArrayList<Problem> problemList  = new ArrayList<Problem>();
+        problemList.add(new Problem());
+        patient.setProblemList(problemList);
+        assertEquals("Should be the ProblemListController we set it to",
+                problemList, patient.getProblemList());
     }
 
     @Test
-    public void testGetProblems(){
+    public void testGetProblemList(){
         Patient patient = new Patient();
-        ArrayList<Problem> problems = new ArrayList<Problem>();
-        Problem problem = new Problem();
-        patient.setProblems(problems);
-        patient.setProblems(problems);
-        assertEquals("Should be the ProblemListController we set it to", problems, patient.getProblems());
+        ArrayList<Problem> problemList = new ArrayList<Problem>();
+        problemList.add(new Problem());
+        patient.setProblemList(problemList);
+        assertEquals("Should be the ProblemListController we set it to", problemList, patient.getProblemList());
+    }
+
+    @Test
+    public void testGetProblem(){
+        Patient patient = new Patient();
+        ArrayList<Problem> problemList  = new ArrayList<Problem>();
+        Problem newProblem = new Problem();
+        problemList.add(newProblem);
+        patient.setProblemList(problemList);       // setProblemList works since it's tested
+        assertEquals("The first item in problemList should be the same as newProblem",
+                newProblem, patient.getProblem(0));
+    }
+
+
+    @Test
+    public void testAddProblem(){
+        Patient patient = new Patient();
+        Problem newProblem = new Problem();
+        patient.addProblem(newProblem);
+        assertEquals("The only problem in problemList should equal to the new problem" +
+                " that was just added", newProblem, patient.getProblem(0));
+
+    }
+
+    @Test
+    public void testSetProblem(){
+        Patient patient = new Patient();
+        Problem oldProblem = new Problem(); Problem newProblem = new Problem();
+        patient.addProblem(oldProblem);
+        patient.setProblem(0, newProblem);
+        assertEquals("The problem at index 0 should equal to the newProblem just got set",
+                newProblem, patient.getProblem(0));
+
+    }
+
+    @Test
+    public void testRemoveProblem(){
+        Patient patient = new Patient();
+        Problem newProblem = new Problem();
+        patient.addProblem(newProblem);
+        patient.removeProblem(newProblem);
+        ArrayList<Problem> emptyList = patient.getProblemList();
+        assertTrue(emptyList.isEmpty());
     }
 }
