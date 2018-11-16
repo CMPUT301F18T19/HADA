e643fe28aed653b86075b0ef9a25a2b648b7b6f3
Jason Robock <robockj@mymacewan.ca>
2018-11-15 20:33:26 -0700

    added floating button stuff

--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/ui/ProblemListActivity.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/ui/ProblemListActivity.java
@@ -2,7 +2,9 @@ package ca.ualberta.cs.cmput301f18t19.hada.hada.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.R;
 
@@ -17,6 +19,13 @@ public class ProblemListActivity extends AppCompatActivity {
 
         TextView username = findViewById(R.id.problemListUsername);
         username.setText(loggedInUser);
+        FloatingActionButton settingFab = (FloatingActionButton) findViewById(R.id.problemListFloatingButton);
+        settingFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO DO THINGS!
+            }
+        });
 
     }
 }
