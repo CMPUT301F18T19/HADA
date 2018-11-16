cdc8d51575d58c86cd61569d6deec95de7e2af9e
Jason Robock <robockj@mymacewan.ca>
2018-11-15 18:42:53 -0700

    EditUserSettingsActivity finished

---
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/ui/EditUserSettingsActivity.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.cmput301f18t19.hada.hada.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ca.ualberta.cs.cmput301f18t19.hada.hada.R;
+
+public class EditUserSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_settings);
+    }
+}
