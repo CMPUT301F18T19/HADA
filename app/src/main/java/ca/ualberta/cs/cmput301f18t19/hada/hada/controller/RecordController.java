7c6fc303eb349dc43b1999d16aefac1661a15de4
Anders Johnson <adj@ualberta.ca>
2018-11-24 12:11:39 -0700

    Added deleteRecord, getRecord, getRecordList

--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/controller/RecordController.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/controller/RecordController.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.cmput301f18t19.hada.hada.controller;
 
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -13,23 +15,39 @@ import ca.ualberta.cs.cmput301f18t19.hada.hada.model.Record;
  */
 public class RecordController {
 
-    public Boolean addRecord(Record record, String parentId){
-        new ESRecordManager.AddRecordTask().execute(record);
-        return true;
+    public Record getRecord(String fileId){
+        try{
+            Record record = new ESRecordManager.GetARecordTask().execute(fileId).get();
+            return record;
+        }catch (Exception e){
+            Log.d("getRecord","Couldn't retrieve record from ES");
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public ArrayList<Record> getRecordList() {
-        ArrayList<Record> recordList = new ArrayList<>();
-        recordList.add(new Record());
-        return recordList;
-}
+    public void addRecord(Record record, String parentId){
+        new ESRecordManager.AddRecordTask().execute(record);
+    }
+
+    public ArrayList<Record> getRecordList(String parentId) {
+        try {
+            return new ESRecordManager.GetRecordListTask().execute(parentId).get();
+        } catch (Exception e) {
+            Log.d("getRecordList","Couldn't retrieve record list from ES");
+            e.printStackTrace();
+        }
+        return null;
+    }
     /**
      * A very specific fundtion for adding comment records to a patient as a CP
      * @author Joe Potentier
     */
-    public Boolean addCommentRecord(int patientIndex, int problemIndex, String comment){
+    public void addCommentRecord(int patientIndex, int problemIndex, String comment){
+        //TODO: Refactor record to better support comments from care provider
+    }
 
-        return true;
-
+    public void deleteRecord(String fileId){
+        new ESRecordManager.DeleteARecordTask().execute(fileId);
     }
 }
