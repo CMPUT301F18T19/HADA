b0c03b13faf03d886d772bf599f0920c4b32739d
Jason Robock <robockj@mymacewan.ca>
2018-11-28 21:03:25 -0700

    changed some things

--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/controller/PhotoController.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/controller/PhotoController.java
@@ -1,8 +1,21 @@
 package ca.ualberta.cs.cmput301f18t19.hada.hada.controller;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+
+import android.util.Base64;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -33,4 +46,45 @@ public class PhotoController {
         record.addPhoto(uri.toString(), "HTTP GO HERE");
         return record;
     }
+    // adapted from https://stackoverflow.com/questions/7111751/uploading-a-photo-via-imgur-on-android-programatically hrickards
+    public String uploadImage(Uri uri){
+        String imageUrl = null;
+        Bitmap bitmap = BitmapFactory.decodeFile(uri.toString());
+
+// Creates Byte Array from picture
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos); // Not sure whether this should be jpeg or png, try both and see which works best
+
+// opens connection and sends data
+        try{
+            URL url = new URL("http://api.imgur.com/3/image");
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            String data = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(Base64.encode(baos.toByteArray(), Base64.DEFAULT).toString(), "UTF-8");
+            data += "&" + URLEncoder.encode("Authorization: Client-ID ", "UTF-8") + "=" + URLEncoder.encode("8d52b0b7c0cd5f1", "UTF-8");
+            wr.write(data);
+            wr.flush();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            conn.getInputStream()));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null)
+                if (inputLine.contains("link")){
+                    imageUrl = inputLine;
+                    return imageUrl;
+                }
+                System.out.println(inputLine);
+            in.close();
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return imageUrl;
+
+}
+
 }
