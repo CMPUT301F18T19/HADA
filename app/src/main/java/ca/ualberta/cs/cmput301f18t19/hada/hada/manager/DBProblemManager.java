32b19dd906a9de514cea2fa8c5e39ecccf842c53
Alex Li <tianyi7@ualberta.ca>
2018-12-01 12:16:30 -0700

    Finsihed DBProblemManager, made minor corrections to DBUserManager.

--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/manager/DBProblemManager.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/manager/DBProblemManager.java
@@ -1,4 +1,217 @@
 package ca.ualberta.cs.cmput301f18t19.hada.hada.manager;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBcontract;
+import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBOpenHelper;
+import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBcontract.problemTable;
+import ca.ualberta.cs.cmput301f18t19.hada.hada.model.Problem;
+
+/**
+ * Manager class that provides an interface for interacting with SQLite database.
+ * Used for saving, loading, deleting problems from/to the database.
+ *
+ * @author Alex
+ * @see Problem
+ * @see DBcontract
+ * @see DBOpenHelper
+ */
 public class DBProblemManager {
+    private Context context;
+    private SQLiteDatabase db;
+    // formatter used to parse LocalDateTime to/from string
+    private DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+
+    /**
+     * Constructor that uses DBOpenHelper to open the database
+     * @param context
+     */
+    public DBProblemManager(Context context){
+        db = new DBOpenHelper(context).getWritableDatabase();
+    }
+
+    /**
+     * Adds a problem to the problemTable
+     * @param problem, problem to be added
+     */
+    public void addProblem(Problem problem) {
+        if (existsProblem(problem.getFileId()))
+            return;
+        ContentValues values = new ContentValues();
+        values.put(problemTable.COL_PARENTID, problem.getParentId());
+        values.put(problemTable.COL_FILEID, problem.getFileId());
+        values.put(problemTable.COL_TITLE, problem.getTitle());
+        values.put(problemTable.COL_DATE, problem.getDate().toString());
+        values.put(problemTable.COL_DESC, problem.getDesc());
+        db.insert(problemTable.TABLE_NAME, null, values);
+    }
+
+    /**
+     * Loads a problem from the problemTable
+     * @param  fileID, problem's fileID
+     * @return problem, null if the problem is not found in the table
+     */
+    public Problem getProblem(String fileID) {
+        Problem problem = null;
+        Cursor c = db.query(
+                problemTable.TABLE_NAME,
+                null,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID },
+                null,
+                null,
+                null
+        );
+        if (c.getCount() > 0) {     // If there is results, else return null
+            c.moveToFirst();        // get the first row, should be the only row
+            problem = new Problem(
+                    c.getString(c.getColumnIndexOrThrow(problemTable.COL_TITLE)),
+                    LocalDateTime.parse(
+                            c.getString(c.getColumnIndexOrThrow(problemTable.COL_DATE)),
+                            formatter),
+                    c.getString(c.getColumnIndexOrThrow(problemTable.COL_DESC))
+            );
+            problem.setParentId(c.getString(c.getColumnIndexOrThrow(problemTable.COL_PARENTID)));
+            problem.setFileId(fileID);
+        }
+        c.close();
+        return problem;
+    }
+
+    /**
+     * Loads all problems of a given patient from the problemTable
+     * @param  patientID, a patient's userID
+     * @return problemList, empty list if no problem is found
+     */
+    public ArrayList<Problem> getProblemList(String patientID) {
+        ArrayList<Problem> problemList = new ArrayList<>();
+        Cursor c = db.query(
+                problemTable.TABLE_NAME,
+                null,
+                problemTable.COL_PARENTID + " = ?",
+                new String[] { patientID },
+                null,
+                null,
+                null
+        );
+        if (c.getCount() > 0) {         // if there is results, else return empty list
+            while (c.moveToNext()) {
+                Problem problem = new Problem(
+                        c.getString(c.getColumnIndexOrThrow(problemTable.COL_TITLE)),
+                        LocalDateTime.parse(
+                                c.getString(c.getColumnIndexOrThrow(problemTable.COL_DATE)),
+                                formatter),
+                        c.getString(c.getColumnIndexOrThrow(problemTable.COL_DESC))
+                );
+                problem.setParentId(patientID);
+                problem.setFileId(c.getString(c.getColumnIndexOrThrow(problemTable.COL_FILEID)));
+                problemList.add(problem);
+            }
+        }
+        c.close();
+        return problemList;
+    }
+
+    /**
+     * Delete a problem with given fileID from the problemTable
+     * @param  fileID, problem's fileID
+     * @return number of rows deleted
+     */
+    public int deleteProblem(String fileID) {
+        int numRowsDeleted = 0;
+        if (!existsProblem(fileID))
+            return numRowsDeleted;
+        numRowsDeleted = db.delete(
+                problemTable.TABLE_NAME,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID }
+        );
+        return numRowsDeleted;
+    }
+
+    /**
+     * edit a problem's title column given the fileID of the problem.
+     * @param fileID a problem's fileID(primary key), newTitle the new title to be set
+     * @return number of rows modified.
+     */
+    public int editProblemTitle(String fileID, String newTitle) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(problemTable.COL_TITLE, newTitle);
+
+        rowsUpdated = db.update(
+                problemTable.TABLE_NAME,
+                values,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID }
+        );
+        return rowsUpdated;
+    }
+
+    /**
+     * edit a problem's date column given the fileID of the problem.
+     * @param fileID a problem's fileID(primary key), newDate a LocalDateTime obj of the new date
+     * @return number of rows modified.
+     */
+    public int editProblemDate(String fileID, LocalDateTime newDate) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(problemTable.COL_DATE, newDate.toString());
+
+        rowsUpdated = db.update(
+                problemTable.TABLE_NAME,
+                values,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID }
+        );
+        return rowsUpdated;
+    }
+
+    /**
+     * edit a problem's description column given the fileID of the problem.
+     * @param fileID a problem's fileID(primary key), newDesc the new description to be set
+     * @return number of rows modified.
+     */
+    public int editProblemDesc(String fileID, String newDesc) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(problemTable.COL_DESC, newDesc);
+
+        rowsUpdated = db.update(
+                problemTable.TABLE_NAME,
+                values,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID }
+        );
+        return rowsUpdated;
+    }
+
+    /**
+     * check if a problem with fileID exists in the database
+     * @param  fileID a problem's fileID
+     * @return true if problem exists, false otherwise
+     */
+    private Boolean existsProblem(String fileID) {
+        Cursor c = db.query(
+                problemTable.TABLE_NAME,
+                null,
+                problemTable.COL_FILEID + " = ?",
+                new String[] { fileID },
+                null,
+                null,
+                null
+        );
+        int count = c.getCount();
+        c.close();
+        return  (count > 0);
+    }
+
 }
--- app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/manager/DBUserManager.java
+++ app/src/main/java/ca/ualberta/cs/cmput301f18t19/hada/hada/manager/DBUserManager.java
@@ -1,27 +1,58 @@
+/* CMPUT 301 - Fall 2018
+ *
+ * Version 1.0
+ *
+ * 2018-10-29
+ *
+ * This is a group project for CMPUT 301 course at the University of Alberta
+ * Copyright (C) 2018  Austin Goebel, Anders Johnson, Alex Li,
+ * Cristopher Penner, Joseph Potentier-Neal, Jason Robock
+ */
+
 package ca.ualberta.cs.cmput301f18t19.hada.hada.manager;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import java.util.ArrayList;
 
+import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBcontract;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBcontract.careProviderTable;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBcontract.patientTable;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.database.DBOpenHelper;
+import ca.ualberta.cs.cmput301f18t19.hada.hada.model.User;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.model.CareProvider;
 import ca.ualberta.cs.cmput301f18t19.hada.hada.model.Patient;
 
+/**
+ * Manager class that provides an interface for interacting with SQLite database.
+ * Used for saving, loading, deleting patients and careProviders from/to the database.
+ *
+ * @author Alex
+ * @see Patient
+ * @see CareProvider
+ * @see User
+ * @see DBcontract
+ * @see DBOpenHelper
+ */
 public class DBUserManager {
     private Context context;
     private SQLiteDatabase db;
 
+    /**
+     * Constructor that uses DBOpenHelper to open the database
+     * @param context
+     */
     public DBUserManager(Context context){
         db = new DBOpenHelper(context).getWritableDatabase();
     }
 
+    /**
+     * Adds a patient to the patientTable
+     * @param patient, patient to be added
+     */
     public void addPatient(Patient patient) {
         if (existsPatient(patient.getUserID()))
             return;
@@ -34,27 +65,37 @@ public class DBUserManager {
         db.insert(patientTable.TABLE_NAME, null, values);
     }
 
+    /**
+     * deletes a patient from the patientTable if it exists, all problems and records
+     * belonging to this patient is also deleted
+     * @param userID, patient's userID
+     * @return number of patients deleted
+     */
     public int deletePatient(String userID) {
         int numRowsDeleted = 0;
         if (!existsPatient(userID))
             return numRowsDeleted;
 
-        String whereClause = patientTable.COL_USERID + " = ?";
-        String[] whereArgs = { userID };
-        numRowsDeleted = db.delete(patientTable.TABLE_NAME, whereClause, whereArgs);
+        numRowsDeleted = db.delete(
+                patientTable.TABLE_NAME,
+                patientTable.COL_USERID + " = ?",
+                new String[] { userID }
+        );
         return numRowsDeleted;
-
     }
 
+    /**
+     * Loads a patient from the patientTable
+     * @param  userID, patient's userID
+     * @return patient, null if the patient is not found in the table
+     */
     public Patient getPatient(String userID) {
         Patient patient = null;
-        String selection = patientTable.COL_USERID + " = ?";
-        String[] selectionArgs = { userID };
         Cursor c = db.query(
                 patientTable.TABLE_NAME,
                 null,
-                selection,
-                selectionArgs,
+                patientTable.COL_USERID + " = ?",
+                new String[] { userID },
                 null,
                 null,
                 null
@@ -71,16 +112,18 @@ public class DBUserManager {
         return patient;
     }
 
+    /**
+     * Loads a list patients for the given careProvider from the patientTable
+     * @param  careProviderID
+     * @return ArrayList<Patient>, empty list if no patient is found in the table
+     */
     public ArrayList<Patient> getPatientList(String careProviderID) {
         ArrayList<Patient> patientList = new ArrayList<>();
-
-        String selection = patientTable.COL_PARENTID + " = ?";
-        String[] selectionArgs = { careProviderID };
         Cursor c = db.query(
                 patientTable.TABLE_NAME,
                 null,
-                selection,
-                selectionArgs,
+                patientTable.COL_PARENTID + " = ?",
+                new String[] { careProviderID },
                 null,
                 null,
                 null
@@ -99,6 +142,10 @@ public class DBUserManager {
         return patientList;
     }
 
+    /**
+     * Adds a careProvider to the careProviderTable
+     * @param  careProvider, careProvider to be added
+     */
     public void addCareProvider(CareProvider careProvider) {
         if (existsCP(careProvider.getUserID()))
             return;
@@ -110,26 +157,36 @@ public class DBUserManager {
         db.insert(careProviderTable.TABLE_NAME, null, values);
     }
 
+    /**
+     * Deletes a CareProvider from the careProviderTable
+     * the careProvider's patients, their problems and records are also removed
+     * @param  userID, careProvider's userID
+     * @return patient, null if the patient is not found in the table
+     */
     public int deleteCareProvider(String userID) {
         int numRowsDeleted = 0;
         if (!existsCP(userID))
             return numRowsDeleted;
-
-        String selection = careProviderTable.COL_USERID + " = ?";
-        String[] selectionArgs = { userID };
-        numRowsDeleted = db.delete(careProviderTable.TABLE_NAME, selection, selectionArgs);
+        numRowsDeleted = db.delete(
+                careProviderTable.TABLE_NAME,
+                careProviderTable.COL_USERID + " = ?",
+                new String[] { userID }
+        );
         return numRowsDeleted;
     }
 
+    /**
+     * loads a CareProvider from the careProviderTable given its userID
+     * @param  userID, careProvider's userID
+     * @return careProvider, null if the careProvider is not found
+     */
     public CareProvider getCareProvider(String userID) {
         CareProvider careProvider = null;
-        String selection = careProviderTable.COL_USERID + " = ?";
-        String[] selectionArgs = { userID };
         Cursor c = db.query(
                 careProviderTable.TABLE_NAME,
                 null,
-                selection,
-                selectionArgs,
+                careProviderTable.COL_USERID + " = ?",
+                new String[] { userID },
                 null,
                 null,
                 null
@@ -146,70 +203,120 @@ public class DBUserManager {
         return careProvider;
     }
 
+    /**
+     * edit a given patient's parentID(careProviderID)
+     * @param  patientID patient's userID, parentID new parentID to be set
+     * @return numbers of rows updated
+     */
+    public int editPatientParentID(String patientID, String parentID) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(patientTable.COL_PARENTID, parentID);
+        rowsUpdated = db.update(
+                patientTable.TABLE_NAME,
+                values,
+                patientTable.COL_USERID + " = ?",
+                new String[] { patientID }
+        );
+        return rowsUpdated;
+    }
+
+    /**
+     * edit a given patient's phone number
+     * @param  patientID patient's userID, newNumber new phone number to be set
+     * @return numbers of rows updated
+     */
+    public int editPatientPhone(String patientID, String newNumber) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(patientTable.COL_PHONE, newNumber);
+        rowsUpdated = db.update(
+                patientTable.TABLE_NAME,
+                values,
+                patientTable.COL_USERID + " = ?",
+                new String[] { patientID }
+        );
+        return rowsUpdated;
+    }
+
+    /**
+     * edit a given patient's email address
+     * @param  patientID patient's userID, newEmail new email address to be set
+     * @return numbers of rows updated
+     */
+    public int editPatientEmail(String patientID, String newEmail) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(patientTable.COL_EMAIL, newEmail);
+        rowsUpdated = db.update(
+                patientTable.TABLE_NAME,
+                values,
+                patientTable.COL_USERID + " = ?",
+                new String[] { patientID }
+        );
+        return rowsUpdated;
+    }
+
+
+    /**
+     * edit a given careProvider's phone number
+     * @param  careProviderID careProvider's userID, newEmail new phone number to be set
+     * @return numbers of rows updated
+     */
+    public int editCareProviderPhone(String careProviderID, String newNumber) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(careProviderTable.COL_PHONE, newNumber);
+        rowsUpdated = db.update(
+                careProviderTable.TABLE_NAME,
+                values,
+                careProviderTable.COL_USERID + " = ?",
+                new String[] { careProviderID }
+        );
+        return rowsUpdated;
+
+    }
+
+    /**
+     * edit a given careProvider's email address
+     * @param  careProviderID careProvider's userID, newEmail new email address to be set
+     * @return numbers of rows updated
+     */
+    public int editCareProviderEmail(String careProviderID, String newEmail) {
+        int rowsUpdated = 0;
+        ContentValues values = new ContentValues();
+        values.put(careProviderTable.COL_EMAIL, newEmail);
+        rowsUpdated = db.update(
+                careProviderTable.TABLE_NAME,
+                values,
+                careProviderTable.COL_USERID + " = ?",
+                new String[] { careProviderID }
+        );
+        return rowsUpdated;
+
+    }
+
+    /**
+     * check if a user with userID exists in the database
+     * @param  userID
+     * @return true if user exists, false otherwise
+     */
     public Boolean userExists(String userID) {
         return (!existsCP(userID) && !existsPatient(userID));
     }
 
-    public void editPatientParentID(String patientID, String parentID) {
-        ContentValues values = new ContentValues();
-        values.put(patientTable.COL_PARENTID, parentID);
 
-        String whereClause = patientTable.COL_USERID + " = ?";
-        String[] whereArgs = { patientID };
-        db.update(patientTable.TABLE_NAME, values, whereClause, whereArgs);
-
-    }
-
-    public void editPatientPhone(String patientID, String newNumber) {
-        ContentValues values = new ContentValues();
-        values.put(patientTable.COL_PHONE, newNumber);
-
-        String whereClause = patientTable.COL_USERID + " = ?";
-        String[] whereArgs = { patientID };
-        db.update(patientTable.TABLE_NAME, values, whereClause, whereArgs);
-
-    }
-
-    public void editPatientEmail(String patientID, String newEmail) {
-        ContentValues values = new ContentValues();
-        values.put(patientTable.COL_EMAIL, newEmail);
-
-        String whereClause = patientTable.COL_USERID + " = ?";
-        String[] whereArgs = { patientID };
-        db.update(patientTable.TABLE_NAME, values, whereClause, whereArgs);
-
-    }
-
-
-    public void editCareProviderPhone(String careProviderID, String newNumber) {
-        ContentValues values = new ContentValues();
-        values.put(careProviderTable.COL_PHONE, newNumber);
-
-        String whereClause = careProviderTable.COL_USERID + " = ?";
-        String[] whereArgs = { careProviderID };
-        db.update(careProviderTable.TABLE_NAME, values, whereClause, whereArgs);
-
-    }
-
-    public void editCareProviderEmail(String careProviderID, String newEmail) {
-        ContentValues values = new ContentValues();
-        values.put(careProviderTable.COL_EMAIL, newEmail);
-
-        String whereClause = careProviderTable.COL_USERID + " = ?";
-        String[] whereArgs = { careProviderID };
-        db.update(careProviderTable.TABLE_NAME, values, whereClause, whereArgs);
-
-    }
-
+    /**
+     * check if a patient with userID exists in the database
+     * @param  userID a patient's userID
+     * @return true if patient exists, false otherwise
+     */
     private Boolean existsPatient(String userID) {
-        String selection = patientTable.COL_USERID + " = ?";
-        String[] selectionArgs = { userID };
-
         Cursor c = db.query(
                 patientTable.TABLE_NAME,
                 null,
-                selection,
-                selectionArgs,
+                patientTable.COL_USERID + " = ?",
+                new String[] { userID },
                 null,
                 null,
                 null
@@ -219,15 +326,17 @@ public class DBUserManager {
         return  (count > 0);
     }
 
+    /**
+     * check if a careProvider with userID exists in the database
+     * @param  userID a careProvider's userID
+     * @return true if careProvider exists, false otherwise
+     */
     private Boolean existsCP(String userID) {
-        String selection = careProviderTable.COL_USERID + " = ?";
-        String[] selectionArgs = { userID };
-
         Cursor c = db.query(
                 careProviderTable.TABLE_NAME,
                 null,
-                selection,
-                selectionArgs,
+                careProviderTable.COL_USERID + " = ?",
+                new String[] { userID },
                 null,
                 null,
                 null
